63093b4a9287b1d04bdc7fa66820c2a16bab9872
EmmaCreativo <52419353+EmmaCreativo@users.noreply.github.com>
2019-07-02 12:05:31 -0600

    Add files via upload

---
+++ js/precarga.js
@@ -0,0 +1,8 @@
+"use strict"
+//------------------------------------precarga
+
+var precarga = document.getElementById('carga');
+
+setTimeout(function cargando() {
+	precarga.style.display = 'none';
+},2000)
